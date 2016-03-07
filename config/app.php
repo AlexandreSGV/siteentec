15bfb6f8da8a5a1679142257c694eb9650d499d8
AlexandreSGV <strapacao@gmail.com>
2016-03-07 19:19:27 -0300

    Alterando a timezone padrão para BRT

--- config/app.php
+++ config/app.php
@@ -218,7 +218,7 @@ return [
             'password' => 'A3de$_Aegypti',
             'database' => 'etiigarassu',
             'encoding' => 'utf8',
-            'timezone' => 'UTC',
+            'timezone' => 'BRT',
             'cacheMetadata' => true,
             'log' => false,
 
@@ -255,7 +255,7 @@ return [
             'password' => 'secret',
             'database' => 'test_myapp',
             'encoding' => 'utf8',
-            'timezone' => 'UTC',
+            'timezone' => 'BRT',
             'cacheMetadata' => true,
             'quoteIdentifiers' => false,
             'log' => false,
